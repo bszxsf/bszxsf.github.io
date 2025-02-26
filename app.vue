17317ac74b5cf7bc06a829e832474ea02d71e863
bszxsf <xushaofengyouxiang@outlook.com>
2025-02-26 21:44:45 +0800

    test: test whether element plus works fine on gh

--- app.vue
+++ app.vue
@@ -1,11 +1,26 @@
 <template>
-  <TopNavigatorBar />
+  <el-container>
+    <el-header>
+      <TopNavigatorBar />
+    </el-header>
+    <el-main>
+     <div>Nuxt3 + ElementPlus样式测试</div>
+     <el-button @click='popup'>弹出窗口</el-button>
+    </el-main>
+  </el-container>
 </template>
 
-<script lang="ts">
-  import TopNavigatorBar from './components/TopNavigatorBar.vue';
+<!-- Note: Build instructions: https://nuxt.com/deploy/github-pages -->
+<!-- npx nuxt build --preset github_pages -->
+<!-- Used to test locally. No need to build before pushing -->
 
-  export default defineComponent({
-    name: "app",
-  });
+<!-- TODO: beginner question: do I have to keep both 'script' blocks if setup syntax used O.o? -->
+<script lang="ts">
+  import TopNavigatorBar from '~/components/TopNavigatorBar.vue';
+</script>
+
+<script lang="ts" setup>
+  let popup = function() {
+    alert("弹出窗口")
+  }
 </script>
--- components/TopNavigatorBar.vue
+++ components/TopNavigatorBar.vue
@@ -1,3 +1,10 @@
 <template>
-  TopNavBar (WIP)
+  <div>
+    <!-- People usually place a logo here, though I don't have one so we put whatever stuff. -->
+    <!-- This icon (or whatever) shall navigate to the home page. So using the home icon ought to be a nice choice... -->
+    <div id="nav-home">
+      <el-icon><House /></el-icon>
+      施工中……
+    </div>
+  </div>
 </template>
