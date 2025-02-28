8ff7a06528e5a0f11a838f2fe23b71436a6c4c99
bszxsf <xushaofengyouxiang@outlook.com>
2025-02-28 12:21:09 +0800

    feat: add side navigator (WIP)

--- app.vue
+++ app.vue
@@ -3,10 +3,16 @@
     <el-header style="margin: 0; padding: 0">
       <TopNavigatorBar />
     </el-header>
-    <el-main>
-      <NuxtPage />
-      <!-- <div>TODO：自适应：左上角一串在屏幕缩小时应当只显示一个home图标；左侧侧边栏在屏幕够小时缩减为一个按钮（现在还一点没加）；搜索栏；根据时间决定加载导航栏时是否启用夜晚模式；github链接</div> -->
-    </el-main>
+    <el-container>
+      <el-aside>
+        <!-- TODO: 宽度自适应 -->
+        <SideNavigator />
+      </el-aside>
+      <el-main>
+        <NuxtPage />
+        <!-- <div>TODO：自适应：左上角一串在屏幕缩小时应当只显示一个home图标；左侧侧边栏在屏幕够小时缩减为一个按钮（现在还一点没加）；搜索栏；根据时间决定加载导航栏时是否启用夜晚模式；github链接</div> -->
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
@@ -16,4 +22,5 @@
 
 <script setup lang="ts">
   import TopNavigatorBar from '~/components/layouts/TopNavigatorBar.vue'
+  import SideNavigator from './components/layouts/SideNavigator.vue'
 </script>
---
+++ components/layouts/SideNavigator.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-menu mode="vertical" router>
+    <el-menu-item-group title="文章">
+      <el-menu-item :disabled="true">
+        空空如也
+        <!-- TODO: 将这个替换为根据实际内容来判断是否显示空 -->
+      </el-menu-item>
+    </el-menu-item-group>
+    <el-menu-item-group title="其他">
+      <el-menu-item index="/">
+        <template #title>
+          <el-icon><HomeFilled /></el-icon>
+          <span>主页</span>
+        </template>
+      </el-menu-item>
+      <el-menu-item index="/about">
+        <template #title>
+          <el-icon><InfoFilled /></el-icon>
+          <span>关于</span>
+        </template>
+      </el-menu-item>
+    </el-menu-item-group>
+  </el-menu>
+</template>
