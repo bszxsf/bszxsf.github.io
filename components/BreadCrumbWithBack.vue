2ed5d6ddb7f472220226a853fc98d6e14c8f1189
bszxsf <xushaofengyouxiang@outlook.com>
2025-02-28 13:24:22 +0800

    feat: complete breadcrumb with back button

--- components/BreadCrumbWithBack.vue
+++ components/BreadCrumbWithBack.vue
@@ -1,7 +1,14 @@
 <template>
-  <div style="display: flex">
-    <div @click="router.back()">返回</div>
-    <div class=""el-divider el-divider--vertical></div>
+  <!-- ElPageHeader is too heavy for this. -->
+  <!-- Note: This component is not designed to be fixed on the top. -->
+  <div style="display: flex;">
+    <div @click="router.back()" class="el-breadcrumb bc-back-btn">
+      <!-- ElBreadcrumb style, including font size, seems hardcoded. -->
+      <!-- We simply have to directly use the class. -->
+      <el-icon><Back /></el-icon>
+      <div style="font-weight: bold; margin-left: calc(var(--el-main-padding) * 0.2);">返回</div>
+    </div>
+    <el-divider direction="vertical" />
     <!-- We don't modify the paths. Path doesn't have an id. No need to use a key. -->
     <el-breadcrumb :separator-icon="ArrowRight">
       <el-breadcrumb-item v-for="path in paths" :to="path.to">
@@ -18,3 +25,14 @@
 
   let { paths } = defineProps<{paths:NamedPaths}>()
 </script>
+
+<style scoped>
+  .bc-back-btn {
+    display: flex;
+    cursor: pointer;
+    line-height: 1;
+    color: var(--el-text-color-primary);
+    transition: var(--el-transition-color);
+    /* consistency with element plus primary style */
+  }
+</style>
