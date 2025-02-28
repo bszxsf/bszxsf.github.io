d85ce703601c44c0eda8f29b2991f545f77b11ad
bszxsf <xushaofengyouxiang@outlook.com>
2025-02-28 11:48:03 +0800

    chore: remove unused comments

--- types/NamedPaths.ts
+++ types/NamedPaths.ts
@@ -5,9 +5,6 @@ declare global {
     name: string // displayed
     to: string   // used by router
   }
-  // Naming tips: RFC specs seem to favor 'path segment' as a term describing what we want
-  // See https://stackoverflow.com/questions/17913224/which-term-best-describes-elements-of-url-path
-  // DO NOT USE DIRECTLY
 
   // Adding functions for this could make it complicated.
   // Plus, it's more flexible when we don't need to have all paths as subpaths of the same path.
