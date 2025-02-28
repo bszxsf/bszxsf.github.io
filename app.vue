01ce02adeeab601769577a4c391f8cc987cae4ed
bszxsf <xushaofengyouxiang@outlook.com>
2025-02-28 21:54:59 +0800

    chore: remove comments

--- app.vue
+++ app.vue
@@ -5,12 +5,10 @@
     </el-header>
     <el-container>
       <el-aside>
-        <!-- TODO: 宽度自适应 -->
         <side-navigator style="height: 100%; padding-top: var(--el-menu-base-level-padding);" />
       </el-aside>
       <el-main>
         <NuxtPage />
-        <!-- <div>TODO：自适应：左上角一串在屏幕缩小时应当只显示一个home图标；左侧侧边栏在屏幕够小时缩减为一个按钮（现在还一点没加）；搜索栏；根据时间决定加载导航栏时是否启用夜晚模式；github链接</div> -->
       </el-main>
     </el-container>
   </el-container>
