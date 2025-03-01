c4ad4427fb34e4211b56d1bc1e32c4f16a33b994
bszxsf <xushaofengyouxiang@outlook.com>
2025-03-02 00:50:42 +0800

    refactor: use nuxt layout

--- app.vue
+++ app.vue
@@ -1,34 +1,5 @@
 <template>
-  <el-container>
-    <el-header style="margin: 0; padding: 0">
-      <navigator-top style="position: fixed; width: 100%; z-index: 10" />
-    </el-header>
-    <el-container style="height: 100%">
-      <el-aside style="width: var(--app-sidebar-width)">
-        <div id="nav-sidebar-scroll">
-          <el-scrollbar>
-            <navigator-side
-              style="
-                height: 100%;
-                padding-top: var(--el-menu-base-level-padding);
-              "
-            />
-          </el-scrollbar>
-        </div>
-      </el-aside>
-      <el-main>
-        <el-backtop />
-        <NuxtPage />
-      </el-main>
-    </el-container>
-  </el-container>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style scoped>
-#nav-sidebar-scroll {
-  position: fixed;
-  width: var(--app-sidebar-width);
-  height: calc(100vh - var(--el-menu-horizontal-height));
-  border-right: 1px solid var(--el-menu-border-color);
-}
-</style>
---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-container>
+    <el-header style="margin: 0; padding: 0">
+      <navigator-top style="position: fixed; width: 100%; z-index: 10" />
+    </el-header>
+    <el-container style="height: 100%">
+      <el-aside style="width: var(--app-sidebar-width)">
+        <div id="nav-sidebar-scroll">
+          <el-scrollbar>
+            <navigator-side
+              style="
+                height: 100%;
+                padding-top: var(--el-menu-base-level-padding);
+              "
+            />
+          </el-scrollbar>
+        </div>
+      </el-aside>
+      <el-main>
+        <el-backtop />
+        <slot />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+#nav-sidebar-scroll {
+  position: fixed;
+  width: var(--app-sidebar-width);
+  height: calc(100vh - var(--el-menu-horizontal-height));
+  border-right: 1px solid var(--el-menu-border-color);
+}
+</style>
