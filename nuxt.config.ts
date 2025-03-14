e4ba41fcb964865654a2acee514763408c88a05e
bszxsf <xushaofengyouxiang@outlook.com>
2025-03-14 20:00:38 +0800

    chore: update packages
    Previous nuxt/content seems buggy with transformers.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-03-14',
   devtools: { enabled: true },
   plugins: ['~/plugins/element-plus'],
   modules: [
