7537c7820280db728dc8bda4dcaae53f68a12e48
bszxsf <xushaofengyouxiang@outlook.com>
2025-03-15 16:17:24 +0800

    feat: add breadcrumb on top of posts

--- pages/posts/[...postid].vue
+++ pages/posts/[...postid].vue
@@ -1,5 +1,8 @@
 <template>
-  <content-renderer v-if="post" :value="post" />
+  <div v-if="post">
+    <bread-crumb-with-back :paths="postPaths" />
+    <content-renderer v-if="post" :value="post" />
+  </div>
   <error-display404 v-else />
 </template>
 
@@ -10,6 +13,16 @@ const postIdArr: string[] = ([] as string[]).concat(route.params.postid);
 if (postIdArr[postIdArr.length - 1] == '') {
   postIdArr.pop();
 }
+let postPaths: NamedPaths = [
+  { name: '主页', to: '/' },
+  { name: '文章', to: '/posts' }
+];
+let cntPathTo = '/posts/';
+for (const postIdSeg of postIdArr) {
+  cntPathTo += postIdSeg;
+  postPaths.push({ name: postIdSeg, to: cntPathTo });
+}
+
 const postIdStr: string = '/' + postIdArr.join('/');
 const postTitleStr: string = postIdArr.reverse().join(' | ');
 
