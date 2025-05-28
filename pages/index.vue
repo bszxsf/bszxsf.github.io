5aeacdf014f735fdcb52f12f34d3a51c4820024f
bszxsf <xushaofengyouxiang@outlook.com>
2025-05-28 17:06:42 +0800

    feat: changed main page a bit

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>这里是索引页。现在这里还没有内容；将来这里会按时间列出文章。</div>
+  <div>这里是索引页。将来这里会列出本站的各类内容。</div>
+  <el-button type="primary" @click="gotoPosts()">点此进入文章页</el-button>
 </template>
 
 <script setup lang="ts">
 useHead({
   title: '主页'
 });
+
+const gotoPosts = () => {
+  return navigateTo('/posts');
+};
 </script>
