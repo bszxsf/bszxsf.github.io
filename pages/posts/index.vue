3a5b210affde2865960cc02aeecd7597ff63d47c
bszxsf <xushaofengyouxiang@outlook.com>
2025-05-28 17:00:49 +0800

    feat: order posts by given key

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,6 +3,22 @@
     <bread-crumb-with-back :paths="postsIndexPaths" />
     <div style="padding: var(--el-main-padding)">
       <h1 style="padding-bottom: var(--el-main-padding)">文章概览</h1>
+      <div style="display: flex">
+        <!-- TODO: Responsive width of this -->
+        <el-select
+          v-model="selectedOrder"
+          value-key="name"
+          style="width: 120px"
+          @change="queryNavItems()"
+        >
+          <el-option
+            v-for="orderOptionPair in orderOptionsPairs"
+            :key="orderOptionPair.name"
+            :label="orderOptionPair.name"
+            :value="orderOptionPair"
+          />
+        </el-select>
+      </div>
       <div class="nav-list-body">
         <div v-for="post of allposts" :key="post.title">
           <post-navigation-item
@@ -20,9 +36,53 @@
 </template>
 
 <script setup lang="ts">
-const { data: allposts } = await useAsyncData(() =>
-  queryCollection('posts').where('published', 'IS NOT NULL').all()
+import type { PageCollections } from '@nuxt/content';
+
+type OrderOptionsPair = {
+  name: string;
+  key: keyof PageCollections['posts'];
+  direction: 'ASC' | 'DESC';
+};
+const orderOptionsPairs: OrderOptionsPair[] = [
+  {
+    name: '默认排序',
+    key: 'id',
+    direction: 'ASC'
+  },
+  {
+    name: '最新发布',
+    key: 'published',
+    direction: 'DESC'
+  },
+  {
+    name: '最新修改',
+    key: 'modified',
+    direction: 'DESC'
+  },
+  {
+    name: '最早发布',
+    key: 'published',
+    direction: 'ASC'
+  }
+];
+
+// Note: Actually this thing is never changed. We only need a one-way bind.
+const selectedOrder: Ref<OrderOptionsPair> = ref(orderOptionsPairs[0]);
+
+const { data: allposts, refresh } = await useAsyncData(
+  () =>
+    queryCollection('posts')
+      .where('published', 'IS NOT NULL')
+      .order(selectedOrder.value.key, selectedOrder.value.direction)
+      .all(),
+  {
+    dedupe: 'cancel'
+  }
 );
+
+const queryNavItems = () => {
+  refresh();
+};
 
 const postsIndexPaths: NamedPaths = [
   { name: '主页', to: '/' },
