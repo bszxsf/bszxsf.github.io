66e5a66255d28844cc1a08cf1c47a64567e3eddc
bszxsf <xushaofengyouxiang@outlook.com>
2025-06-05 09:28:40 +0800

    feat: refine ProseImg a bit
    Change attribute names
    Respect `alt`

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -5,16 +5,18 @@
     <!-- Some prose style sets a margin for this, a big one. Fortunately it also provided with a class to disable it. -->
     <el-image
       loading="lazy"
-      :src="src"
-      :fit="fit"
+      :src
+      :fit
       ref="imageContainer"
       :style="imageContainerStyle"
       style="display: block; margin: 0 auto"
+      :title
     >
       <template #error>
         <div class="img-err" :style="imageContainerStyle">
           <!-- height: 100% does not work. Even official examples have the same issue... Sigh. -->
           <el-icon><el-icon-picture /></el-icon>
+          <span v-if="alt">{{ alt }}</span>
         </div>
       </template>
     </el-image>
@@ -37,34 +39,38 @@
 <script setup lang="ts">
 const {
   src,
+  alt,
+  title,
   caption,
-  imgWidth,
-  imgMinWidth = '50px',
-  imgMaxWidth = '100%',
-  imgHeight,
-  imgMinHeight = '50px',
-  imgMaxHeight = '100%',
+  width,
+  height,
+  minWidth = '50px',
+  maxWidth = '100%',
+  minHeight = '50px',
+  maxHeight = '100%',
   fit = 'contain'
 } = defineProps<{
   src: string;
+  alt?: string;
+  title?: string;
   caption?: string;
-  imgWidth?: CssLength;
-  imgMinWidth?: CssLength;
-  imgMaxWidth?: CssLength;
-  imgHeight?: CssLength;
-  imgMinHeight?: CssLength;
-  imgMaxHeight?: CssLength;
+  width?: CssLength;
+  height?: CssLength;
+  minWidth?: CssLength;
+  maxWidth?: CssLength;
+  minHeight?: CssLength;
+  maxHeight?: CssLength;
   fit?: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down';
 }>();
 
 // Things obtained via useTemplateRef() is available after mount, which is too late for SSR.
 let imageContainerStyle: string =
-  'min-width: ' + imgMinWidth + '; max-width: ' + imgMaxWidth;
+  'min-width: ' + minWidth + '; max-width: ' + maxWidth;
 let captionStyle: string = imageContainerStyle;
 imageContainerStyle +=
-  '; min-height: ' + imgMinHeight + '; max-height: ' + imgMaxHeight;
-if (imgWidth != undefined) imageContainerStyle += '; width: ' + imgWidth;
-if (imgHeight != undefined) imageContainerStyle += '; height: ' + imgHeight;
+  '; min-height: ' + minHeight + '; max-height: ' + maxHeight;
+if (width != undefined) imageContainerStyle += '; width: ' + width;
+if (height != undefined) imageContainerStyle += '; height: ' + height;
 </script>
 
 <style scoped>
@@ -79,5 +85,8 @@ if (imgHeight != undefined) imageContainerStyle += '; height: ' + imgHeight;
   background: var(--el-fill-color-light);
   color: var(--el-text-color-secondary);
   display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
 }
 </style>
