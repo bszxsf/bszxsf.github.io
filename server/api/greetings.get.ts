1978c413d6afe3de50849f0ee6d7891f4f49ea5f
bszxsf <xushaofengyouxiang@outlook.com>
2025-06-17 00:07:26 +0800

    chore: remove test code
    This reverts commit 3b8ce7d0e02c4373c525aed7fc98e83108fcadf9.
    
    Okay, we can't deploy server APIs on github pages, as expected.

--- server/api/greetings.get.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const sections = await queryCollectionSearchSections(event, 'posts');
-  event.node.res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  return 'hello, we have: ' + JSON.stringify(sections);
-});
