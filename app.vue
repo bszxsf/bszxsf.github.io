ddf3ebe10b44a9e2db723db2738714a950fc05d6
bszxsf <xushaofengyouxiang@outlook.com>
2025-07-14 13:57:01 +0800

    refactor: move reusable code to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,32 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <el-container style="height: 100%">
+    <el-header style="margin: 0; padding: 0">
+      <el-affix>
+        <navigator-top style="width: 100%; z-index: var(--el-index-top)" />
+      </el-affix>
+    </el-header>
+    <el-container
+      style="height: calc(100vh - var(--el-menu-horizontal-height))"
+    >
+      <el-aside style="width: var(--app-sidebar-width)">
+        <div id="nav-sidebar-scroll">
+          <el-scrollbar>
+            <navigator-side
+              style="
+                height: 100%;
+                padding-top: var(--el-menu-base-level-padding);
+              "
+            />
+          </el-scrollbar>
+        </div>
+      </el-aside>
+      <el-main id="app-main-container">
+        <nuxt-layout>
+          <nuxt-page />
+        </nuxt-layout>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script setup lang="ts">
@@ -11,3 +36,15 @@ useHead({
   }
 });
 </script>
+
+<style scoped>
+#nav-sidebar-scroll {
+  position: fixed;
+  width: var(--app-sidebar-width);
+  height: calc(100vh - var(--el-menu-horizontal-height));
+  border-right: 1px solid var(--el-menu-border-color);
+}
+#app-main-container {
+  padding: 0;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,18 @@
 <template>
-  <el-container style="height: 100%">
-    <el-header style="margin: 0; padding: 0">
-      <el-affix>
-        <navigator-top style="width: 100%; z-index: var(--el-index-top)" />
-      </el-affix>
-    </el-header>
-    <el-container
-      style="height: calc(100vh - var(--el-menu-horizontal-height))"
-    >
-      <el-aside style="width: var(--app-sidebar-width)">
-        <div id="nav-sidebar-scroll">
-          <el-scrollbar>
-            <navigator-side
-              style="
-                height: 100%;
-                padding-top: var(--el-menu-base-level-padding);
-              "
-            />
-          </el-scrollbar>
-        </div>
-      </el-aside>
-      <el-main
-        id="app-content-frame"
-        style="display: flex; flex-direction: column"
-      >
-        <el-backtop target="#app-content-frame" />
-        <div style="flex-grow: 1">
-          <slot />
-        </div>
-        <footer-license-claim />
-      </el-main>
-    </el-container>
-  </el-container>
+  <div id="app-content-frame">
+    <el-backtop target="#app-content-frame" />
+    <div style="flex-grow: 1">
+      <slot />
+    </div>
+    <footer-license-claim />
+  </div>
 </template>
 
 <style scoped>
-#nav-sidebar-scroll {
-  position: fixed;
-  width: var(--app-sidebar-width);
-  height: calc(100vh - var(--el-menu-horizontal-height));
-  border-right: 1px solid var(--el-menu-border-color);
+#app-content-frame {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: var(--el-main-padding);
 }
 </style>
